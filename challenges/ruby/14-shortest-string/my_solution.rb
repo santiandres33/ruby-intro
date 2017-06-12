c20c3ad8c8bc1ed1e4fb4c55398b3b04129f5aab
santiandres33 <santiandres33@hotmail.com>
2017-06-11 19:47:24 -0500

    ruby-intro 14

--- challenges/ruby/14-shortest-string/my_solution.rb
+++ challenges/ruby/14-shortest-string/my_solution.rb
@@ -2,12 +2,29 @@
 
 #Tu solucion abajo:
 
-def shortest_string(array)
-  return nil if array.empty?
-  array.sort_by! {|x| x.length}
-  array.first
+def shortest_string(strings_array)
+	if strings_array == nil || strings_array == []
+		return nil
+	else
+		menor = 1000000
+		menor_res = ""
+		strings_array.each do |elem|
+			length = elem.length
+			if length < menor
+				menor = length
+				menor_res = elem
+			end
+		end
+		return menor_res #puts menor_res
+	end
 end
 
-def shortest_string(array)
-  array.min({|a,b| a.length <=> b.length})
-end
+# # 'cat' is 3 characters long
+# shortest_string(['cat', 'zzzzzzz', 'apples'])  # => "cat"
+
+# # '' is 0 characters long, but it's the only string
+# shortest_string(['']) # => ''
+
+# shortest_string([]) # => ni
+
+# shortest_string(['cTSaeeeeeeedfa', 'zzzzeeeezzz', 'apples','zzzeezzzz', 'app'])
