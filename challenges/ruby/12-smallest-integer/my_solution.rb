7ad54b2e12bb143bed0fd28e1f06d30a62c3b714
santiandres33 <santiandres33@hotmail.com>
2017-06-11 19:02:25 -0500

    intro-ruby-13

--- challenges/ruby/12-smallest-integer/my_solution.rb
+++ challenges/ruby/12-smallest-integer/my_solution.rb
@@ -6,3 +6,38 @@
 
 
 
+
+# def smallest_integer ( integers_array)
+# 	integers_array.each do |elem|
+# 		menor = elem ##problema aca
+# 		if elem < menor
+# 			menor = elem
+
+# 		end
+# 		puts menor
+# 		return menor
+# 	end
+# end
+
+def smallest_integer (integers_array)
+	if integers_array == "" || integers_array == nil
+		#puts "nil"
+		return nil
+	else
+	smallest = integers_array.min 
+	#puts smallest
+	end
+end
+
+
+# a1 = [1, 2, 3]
+# a2 = [-10, -20, -30]
+# smallest_integer(a1)
+# puts "--"
+# smallest_integer(a2)
+# puts "--"
+# smallest_integer([0, -10, 10])
+# puts "--"
+# smallest_integer("")
+# puts "--"
+# smallest_integer(nil)
