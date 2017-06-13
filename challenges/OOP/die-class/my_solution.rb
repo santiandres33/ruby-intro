bcb3452ae533c8b1d54fc905b6025781c0521132
santiandres33 <santiandres33@hotmail.com>
2017-06-12 20:43:55 -0500

    oop - dice

--- challenges/OOP/die-class/my_solution.rb
+++ challenges/OOP/die-class/my_solution.rb
@@ -4,6 +4,32 @@
 
 # 1. Solucion Inicial
 
+class Die
+
+	attr_reader :sides
+
+	def initialize(sides)  #sides: argumento
+		if sides < 1
+			raise ArgumentError.new("numero menor a 0")
+		else
+		@sides = sides #@sides:  variable de instacia ----- sides: valor del argumento
+	end 
+end
+
+
+
+def roll()
+	1 + rand(@sides)
+end
+
+end
+
+die = Die.new(6)
+die = Die.new(1)
+die = Die.new(8)
+die = Die.new(1)
+die = Die.new(6)
+die = Die.new(6)
 
 
 
